d9386476849a576f9d32f41b1311f1917af85735
kevin <kevin.antoine@insa-lyon.fr>
2015-05-03 22:17:55 +0200

    node fix

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var logger = require('express-logger');
-var app = express();
-app.use(logger({path: "logs/log.txt"}));
-// app.use(logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
-console.log("Listening on port 5000");
-console.log("https://localhost:5000");
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+});
+
+server.listen(8000);
