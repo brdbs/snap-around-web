bde1f8cd8fea421695c71006934dd680efc51ec0
kevin <kevin.antoine@insa-lyon.fr>
2015-05-03 23:07:50 +0200

    added finalhandler and serve-static to package.json

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-// var http = require('http');
+var http = require('http');
 
-// var finalhandler = require('finalhandler');
-// var serveStatic = require('serve-static');
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
 
-// var serve = serveStatic("./");
+var serve = serveStatic("./");
 
-// var server = http.createServer(function(req, res){
-//   var done = finalhandler(req, res)
-//   serve(req, res, done)
-// });
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
 
-// var port = process.env.PORT || 3000;
-// app.listen(port, function() {
-// 	console.log("Server running");
-// }
-
-function main(argv) {
-  new HttpServer({
-    'GET': createServlet(StaticServlet),
-    'HEAD': createServlet(StaticServlet)
-  }).start(process.env.PORT || Number(argv[2]) || DEFAULT_PORT);
-}
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+	console.log("Server running");
+});
