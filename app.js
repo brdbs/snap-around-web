3f5b3514fb6b15be3a5b822dc0e8ef9dc7ffd164
kevin <kevin.antoine@insa-lyon.fr>
2015-05-03 23:48:40 +0200

    cors

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-var http = require('http');
+// var http = require('http');
 
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
+// var finalhandler = require('finalhandler');
+// var serveStatic = require('serve-static');
 
-var serve = serveStatic("./");
+// var serve = serveStatic("./");
 
-var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  serve(req, res, done);
-});
+// var server = http.createServer(function(req, res){
+//   var done = finalhandler(req, res);
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   serve(req, res, done);
+// });
 
-var port = process.env.PORT || 3000;
-server.listen(port, function() {
-	console.log("Server running");
-});
+
+// var connect = require("connect");
+
+// var server = connect().use(connect.static( '/'));
+
+// var port = process.env.PORT || 3000;
+// server.listen(port, function() {
+// 	console.log("Server running");
+// });
+
+var connect = require('connect');
+var serveStatic = require('serve-static'); 
+var app = connect(); 
+
+app.use(serveStatic('./')); 
+
+app.listen(5000);
