91a225cb69083324c10c6db26a4cb5a78bdfc9ea
kevin <kevin.antoine@insa-lyon.fr>
2015-05-03 23:43:52 +0200

    cors

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ var serve = serveStatic("./");
 
 var server = http.createServer(function(req, res){
   var done = finalhandler(req, res);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   serve(req, res, done);
 });
-  server.serve(function (req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      next();
-  });
 
 var port = process.env.PORT || 3000;
 server.listen(port, function() {
