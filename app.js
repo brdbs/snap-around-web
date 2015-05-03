32d5869064d6ee5987c8841393f6c01b231fe493
kevin <kevin.antoine@insa-lyon.fr>
2015-05-03 23:41:52 +0200

    cors

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var server = http.createServer(function(req, res){
   var done = finalhandler(req, res);
   serve(req, res, done);
 });
+  server.serve(function (req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      next();
+  });
 
 var port = process.env.PORT || 3000;
 server.listen(port, function() {
