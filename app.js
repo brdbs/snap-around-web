c8486156aa6f283d9a7b369f6003fc5781953a34
kevin <kevin.antoine@insa-lyon.fr>
2015-05-06 22:08:46 +0200

    changed features

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// var http = require('http');
-
-// var finalhandler = require('finalhandler');
-// var serveStatic = require('serve-static');
-
-// var serve = serveStatic("./");
-
-// var server = http.createServer(function(req, res){
-//   var done = finalhandler(req, res);
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   serve(req, res, done);
-// });
-
-
-// var connect = require("connect");
-
-// var server = connect().use(connect.static( '/'));
-
-// 
 
 var connect = require('connect');
 var serveStatic = require('serve-static'); 
--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,5 @@ var app = angular.module("snap-around",['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'partials/index.html',
-  }).when('/dashboard', {
-    templateUrl: 'partials/dashboard.html',
   })
 }]);
