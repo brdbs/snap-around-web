10e7042a4d4d6eb133e7062286ddf7188122bbbd
kevin <kevin.antoine@insa-lyon.fr>
2015-05-14 20:07:55 +0200

    added support page

--- scripts/app.js
+++ scripts/app.js
@@ -5,5 +5,7 @@ var app = angular.module("snap-around",['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'partials/index.html',
+  }).when('/support', {
+    templateUrl: 'partials/support.html',
   })
 }]);
